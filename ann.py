9e526ebd6b7ffca2170aa3cbbd14fed4d8b22bad
sposito <sposito.thiago@gmail.com>
2018-04-30 15:38:54 +0100

    import fix _

--- ann.py
+++ ann.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.models import model from json
-from keras.models import model from yaml
+from keras.models import model_from_json
+from keras.models import model_from_yaml
 import matplotlib.pyplot as plt
