56c432a5d59c4bb9547cdaf80ee157261cc84c29
sposito <sposito.thiago@gmail.com>
2018-04-30 15:31:23 +0100

    libImports

---
+++ ann.py
@@ -0,0 +1,6 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import model from json
+from keras.models import model from yaml
+import matplotlib.pyplot as plt
